d97a30c2bb0cd5ae4b1e359ecdcbcf059cc1018e
skyfly33 <skyfly33727@gmail.com>
2015-08-23 23:48:15 +0900

    add auto mapper scan(UserDao) query findAllUser.

--- src/main/java/com/skyfly33/spring/dao/UserDao.java
+++ src/main/java/com/skyfly33/spring/dao/UserDao.java
@@ -1,10 +1,15 @@
 package com.skyfly33.spring.dao;
 
+import com.skyfly33.spring.domain.User;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by donghoon on 15. 8. 22..
  */
 @Repository
-public class UserDao {
+public interface UserDao {
+
+    List<User> findAllUsers();
 }
