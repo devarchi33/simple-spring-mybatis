ffebd216b3cf29e9b06f44d624947265d45a5feb
skyfly33 <imfly7@naver.com>
2015-08-25 19:32:48 +0900

    add whoo controller.

---
+++ src/main/java/com/skyfly33/spring/controller/WHOOController.java
@@ -0,0 +1,24 @@
+package com.skyfly33.spring.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by donghoon on 15. 8. 25..
+ */
+@Controller
+@RequestMapping("/whoo")
+public class WHOOController {
+
+    Logger logger = LoggerFactory.getLogger(WHOOController.class);
+
+    @RequestMapping("/setKakaoAuthCode")
+    public String setAuthCode(@RequestBody String kakaoAuthCode) {
+        logger.info("kakaoAuthCode" + kakaoAuthCode);
+        return kakaoAuthCode;
+    }
+}
