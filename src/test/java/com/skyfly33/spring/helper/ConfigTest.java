d84489b392896ee92dd1c32d9a45c9f8e78946d6
skyfly33 <skyfly33727@gmail.com>
2015-08-31 10:51:18 +0900

    add helper class(properties read) & unit test.

---
+++ src/test/java/com/skyfly33/spring/helper/ConfigTest.java
@@ -0,0 +1,31 @@
+package com.skyfly33.spring.helper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by donghoon on 15. 8. 31..
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/pojoApplicationContext.xml"})
+public class ConfigTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigTest.class);
+
+    @Autowired
+    Config whooConfig;
+
+    @Test
+    public void configReadTest() {
+        String dburl = whooConfig.getProperties("db.url");
+        logger.info("Result: " + dburl);
+        assertEquals("jdbc:mysql://192.168.0.225", dburl);
+    }
+}
