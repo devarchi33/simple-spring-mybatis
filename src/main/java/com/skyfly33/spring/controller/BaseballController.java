2b9f565251c8f3bc8c969a6a95ef78d2fc178532
skyfly33 <skyfly33727@gmail.com>
2015-09-13 16:00:24 +0900

    add baseball controller.

--- src/main/java/com/skyfly33/spring/controller/BaseballController.java
+++ src/main/java/com/skyfly33/spring/controller/BaseballController.java
@@ -5,16 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by donghoon on 15. 8. 25..
  */
 @Controller
-@RequestMapping("/whoo")
-public class WHOOController {
+@RequestMapping("/sports/baseball")
+public class BaseballController {
 
-    Logger logger = LoggerFactory.getLogger(WHOOController.class);
+    Logger logger = LoggerFactory.getLogger(BaseballController.class);
 
     @RequestMapping("/setKakaoAuthCode")
     public String setAuthCode(@RequestBody String kakaoAuthCode) {
